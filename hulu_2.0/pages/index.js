1638bd82d9e23bd0ad9694e7bbc3da6131273986
Philip Beauford <PhilipBeauford@github.com>
2021-12-29 00:55:38 -0800

    initial commit

---
+++ hulu_2.0/pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Hulu 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+    <Header />
+
+    <Nav />
+
+    {/* Results */}
+
+
+
+    </div>
+  )
+}
