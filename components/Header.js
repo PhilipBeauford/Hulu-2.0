da78bef955d0e8294865983a3359bdecfd0f54e8
Philip Beauford <PhilipBeauford@github.com>
2021-12-29 20:05:40 -0800

    links

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,15 @@ function Header() {
     return (
         <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
             <div className="flex flex-grow justify-evenly max-w-2xl">
-                <HeaderItem title='HOME'Icon={HomeIcon}/>
-                <HeaderItem title='TRENDING'Icon={LightningBoltIcon}/>
+                <a href="./">
+                    <HeaderItem title='HOME'Icon={HomeIcon}/>
+                </a>
+                
+                <a href="/?genre=fetchTrending">
+                   <HeaderItem title='TRENDING'Icon={LightningBoltIcon}/> 
+                </a>
+                
+
                 <HeaderItem title='VERIFIED'Icon={BadgeCheckIcon}/>
                 <HeaderItem title='COLLECTIONS'Icon={CollectionIcon}/>
                 <HeaderItem title='SEARCH'Icon={SearchIcon}/>
