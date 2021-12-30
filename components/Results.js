b67bd9a1dfdcf44108376654cac694fd665cb7d5
Philip Beauford <PhilipBeauford@github.com>
2021-12-29 20:35:47 -0800

    finalized

--- components/Results.js
+++ components/Results.js
@@ -6,8 +6,7 @@ function Results( { results }) {
 
 
     return (
-        <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 
-                        3xl:grid-cols'>
+        <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5'>
 
             {results.map(result => (
                 <Thumbnail key={result.id} result={result} />
